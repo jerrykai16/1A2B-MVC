b11dde0a9ca2fe141c76f28db6e919e1a70676d5
Jerrykai <k86887961@gmail.com>
2023-09-11 19:42:24 +0800

    完成數字選擇 (遊戲的答案)

--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -7,10 +7,25 @@ use Illuminate\Http\Request;
 class BooksController extends Controller
 {
     // index
-    public function index() {
+    public function index(){
         return view("index");
     }
 
+    public function guess(){
+
+        return view("index");
+    }
+    public function img(){
+        $unique_num = [];
+        while(count($unique_num)<4){
+            $num = mt_rand(0,9);
+            if(!in_array($num,$unique_num)){                
+                $unique_num[] = $num;
+            }
+        }        
+
+        return view("index",['unique_num' => $unique_num]);
+    }
 
 
 }
